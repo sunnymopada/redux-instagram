aa4adf7c929dd6f58c392fed17fb03963f7de31b
Sunny <sanyasirao@ibhubs.co>
2021-06-01 20:35:11 +0530

    Added redux store

---
+++ client/components/PhotoGrid.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+class PhotoGrid extends Component {
+  render() {
+    return (
+      <div className='photo-grid'>
+        <p>I'm photo grid</p>
+      </div>
+    )
+  }
+}
+
+export default PhotoGrid
---
+++ client/components/Single.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+class Single extends Component {
+  render() {
+    return (
+      <div className='single-photo'>
+        <p>I'm single</p>
+      </div>
+    )
+  }
+}
+
+export default Single
