a0634ffb0688b448163732aa244eb28ec771e5a7
Sunny <sanyasirao@ibhubs.co>
2021-06-02 10:40:38 +0530

    Add functionality for add comment, remove comment

--- client/actions/actionCreator.js
+++ client/actions/actionCreator.js
@@ -14,10 +14,10 @@ export function addComment(postId, author, comment) {
   }
 }
 
-export function removeComment(postId, index) {
+export function removeComment(postId, i) {
   return {
     type: 'REMOVE_COMMENT',
-    index,
+    i,
     postId,
   }
 }
